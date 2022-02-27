f49034326d7390e437fd845a4a41df99a3975519
nnnn8888 <ndupui@gmail.com>
2022-02-27 19:44:37 +0100

    ajout like et dollars

--- fetch.js
+++ fetch.js
@@ -9,19 +9,40 @@ const client = new TwitterApi({
     accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
+const reseachtwitt = async ()=>{
+    await client.v2.search('#crypto', { 
+        'max_results': '40',
+        'expansions': 'author_id'
+     }).then((val) => {
+    //console.log(val._realData.data[0])
+    for (let element in val._realData.data) {         
+        ajoutlike = val._realData.data[element].id;
+        console.log(ajoutlike)
+        client.v2.like(process.env.TWITTER_ID, ajoutlike).then((result) => {
+            console.log(result)
+            console.log("success")})
+            .catch(err => {
+            console.error(err)
+        })
+        }
+})}
+
 const getPriceAndTweet = () => {
     fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10')
         .then(res => res.json())
         .then(json => {
             
             const today = new Date();
+            
+            let moins1 = today.setHours(today.getHours() - 1);
+            
             let date = today.toUTCString();
 
 
             let letweet = "";
 
             for (let item in json) {         
-            letweet = letweet + `$${json[item].symbol.toUpperCase()}: ${json[item].current_price} | `;
+            letweet = letweet + `$${json[item].symbol.toUpperCase()}: ${json[item].current_price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })} | `;
             }
 
             finaltt = date + ' - TOP 10 - ' + letweet + ' #crypto #blockchain #token #coin';
@@ -33,6 +54,8 @@ const getPriceAndTweet = () => {
                 .catch(err => {
                 console.error(err)
             })
+
+            reseachtwitt()
 
         .catch(err => {
             console.error(err)
